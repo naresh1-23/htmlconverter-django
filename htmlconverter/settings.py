1456ffe6a5a17162c686681ac466863bedc68e53
naresh1-23 <nareshtamang20000@gmail.com>
2023-05-23 21:00:40 +0545

    practice

--- htmlconverter/settings.py
+++ htmlconverter/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_&(p3%5h(asztpayi0z%s81ixme)^%$7$@+(7fvg6a&2pdai%u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
